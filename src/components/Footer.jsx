820f1d261969a77a25a34b4e43c4bc359d87e1f3
MuhammadTalha15 <talhaz5518@gmail.com>
2024-11-21 21:15:49 +0500

    Full Layout Update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import '../styles/footer.css'
+import Logo from '../assets/Logo/footer-Logo.png'
+import Facebook from '../assets/Logo/facebook.svg'
+import Instagram from '../assets/Logo/instagram.svg'
+import Github from '../assets/Logo/github.svg'
+import Stack from '../assets/Logo/stack-overflow.svg'
+import { Link, useLocation } from 'react-router-dom'
+
+const Footer = () => {
+  const date = new Date();
+  let currentYear = date.getFullYear();
+
+  const path = useLocation();
+  useEffect(() => {
+    scrollFooterTop();
+  }, [path]);
+
+  const scrollFooterTop = () => {
+    window.scrollTo(0, 0);
+  }
+
+  return (
+    <>
+      <section className="footer">
+        <div className="uppr-txt-cnt">
+          <div className="txt-cnt">
+            <p className="ftr-upr-txt">Your Vission, My Expertise Feel free to Contact for your Solutions and Products.</p>
+          </div>
+        </div>
+        <div className="ftr-lgo-cnt">
+          <img className='ftr-lgo' src={Logo} alt="Footer Logo" />
+        </div>
+        <div className="ftr-nav-cnt">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/About">About</Link></li>
+            <li><Link to="/Projects">Projects</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+          </ul>
+        </div>
+        <div className="ftr-scl-lnk-cnt">
+          <ul className='scl'>
+            <li><a href="https://www.facebook.com/profile.php?id=100092518021987" target='_blank'><img className='ftr-scl-img' src={Facebook} alt="" /></a></li>
+            <li><a href="https://www.instagram.com/ttlha._15/" target='_blank'><img className='ftr-scl-img' src={Instagram} alt="" /></a></li>
+            <li><a href="https://github.com/MuhammadTalha15" target='_blank'><img className='ftr-scl-img' src={Github} alt="" /></a></li>
+            <li><a href="https://stackoverflow.com/users/23203699/muhammad-talha" target='_blank'><img className='ftr-scl-img' src={Stack} alt="" /></a></li>
+          </ul>
+        </div>
+        <div className="cpy-rht-cnt">
+          <p className="cpy-rgt-txt"> Copyright © {currentYear} Website | Muhammad Talha®. All rights reserved. </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,35 +2,111 @@ import React from 'react'
 import '../styles/home.css'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import { Link } from 'react-router-dom';
+import ServiceCard from './serviceCard';
+import ProjectCard from './projectCard'
+import Footer from './Footer'
+
+import Trophy from '../assets/Icons/Trophy.png'
+import Bug from '../assets/Icons/Bug.png'
+import Light from '../assets/Icons/lightning.png'
+import Clock from '../assets/Icons/Clock.png'
+
+import JavascriptLogo from '../assets/Logo/js.png'
+import PythonLogo from '../assets/Logo/py.png'
+
+import Facebook from '../assets/Logo/facebook.svg'
+import Instagram from '../assets/Logo/instagram.svg'
+import Github from '../assets/Logo/github.svg'
+import Stack from '../assets/Logo/stack-overflow.svg'
 
 const Home = () => {
 
     const [text] = useTypewriter({
-        words:['Computer Science Student.', 'Web Developer.', 'Programmer.'],
+        words: ['Computer Science Student.', 'Web Developer.', 'Programmer.'],
         loop: {},
         typeSpeed: 100,
         deleteSpeed: 100,
         delaySpeed: 2000,
     });
-  return (
-    <>
-        <section className="hero">
-            <div className="cont-cnt">
-                <h1 className="greet">Hello, I'm</h1>
-                <h2 className="name">Muhammad Talha</h2>
-                <h2 className="prof">Web Developer</h2>
-                <p className="des">Experienced Web Developer, Designer. Programmer, SEO Expert and a Passionate Student.</p>
-                <p className="note">Currently Seeking for Freelance Projects</p>
-                <div className="btn-cnt">
-                    <Link to='#' target='_blank' className='active hero-btn'>Resume</Link>
-                    <Link to='#' target='_blank' className='hero-btn'>Explore Work</Link>
+    return (
+        <>
+            <section className="hero">
+                <div className="cont-cnt">
+                    <h1 className="greet">Hello, I am</h1>
+                    <h2 className="name">Muhammad Talha</h2>
+                    <h2 className="prof">Web Developer</h2>
+                    <p className="des">Experienced Web Developer, Designer. Programmer, SEO Expert and a Passionate Student.</p>
+                    <p className="note">Currently Seeking for Freelance Projects</p>
+                    <div className="btn-cnt">
+                        <Link to='#' target='_blank' className='active hero-btn'>Resume</Link>
+                        <Link to='#' target='_blank' className='hero-btn'>Explore Work</Link>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <section className='sec-section'></section>
-    </>
-  )
+            <section className='sec-section'>
+                <h1 className="sec-section-sub-head">SERVICES</h1>
+                <h2 className="sec-section-mn-head">Exceeding Expectations</h2>
+                <p className="sec-section-des">Offering bespoke web development solutions that blend innovation with precision to elevate your digital presence.</p>
+                <div className="crd-cnt">
+                    <ServiceCard image={Trophy} heading={'Expertise in Figma to Websites'} description={'Seamlessly transform your designs from Figma to fully functional websites with my expertise, ensuring pixel-perfect precision and responsive interactivity.'} />
+                    <ServiceCard image={Bug} heading={'Comprehensive Testing'} description={'Ensure your projects are robust and error-free with my comprehensive testing services, covering functionality, performance, and user experience.'} />
+                    <ServiceCard image={Light} heading={'Interactivity and Responsiveness'} description={'Enhance user experience with dynamic interactivity and seamless responsiveness, ensuring your website looks and performs flawlessly on all devices.'} />
+                    <ServiceCard image={Clock} heading={'Product Delivery on Time'} description={'Depend on timely and efficient product delivery, ensuring your projects are completed and launched as scheduled without compromising quality.'} />
+                </div>
+            </section>
+
+            <section className="thir-section">
+                <h1 className="sec-section-sub-head">PROJECTS</h1>
+                <h2 className="sec-section-mn-head">My Latest Projects</h2>
+                <p className="sec-section-des">Discover My Latest Creations that showcase the Designing Innovations and Digital Essentials. Feel free to Explore for More.</p>
+                <div className="prjct-crd-cnt">
+                    <ProjectCard projectLink={'https://www.github.com'} image={JavascriptLogo} projectHeading='Todo List App' projectDescription='A sleek and efficient to-do list app built with vanilla JavaScript, designed to help you manage tasks effortlessly with a clean and intuitive interface.' />
+                    <ProjectCard projectLink={'https://www.github.com'} image={JavascriptLogo} projectHeading='Tic Tac Toe Game' projectDescription='A classic Tic Tac Toe game created in vanilla JavaScript, offering a smooth and interactive experience with a simple, user-friendly interface.' />
+                    <ProjectCard projectLink={'https://www.github.com'} image={PythonLogo} projectHeading='QR Code Generator' projectDescription='A robust QR code generator built with Python, enabling you to create customized QR codes quickly and easily.' />
+                </div>
+            </section>
+
+            <section className='frth-section'>
+                <h1 className="sec-section-sub-head">CONTACT</h1>
+                <h2 className="sec-section-mn-head">Get In Touch</h2>
+                <p className="sec-section-des">Reach out to collaborate or discuss innovative web solutions—I'm here to connect.</p>
+                <div className="contact-cnt">
+                    <p className="contact-des">
+                        Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at <a className='contact-des-a' href="https://mail.google.com">talhaz5518@gmail.com</a>
+                    </p>
+
+                    <div className="lnk-cnt">
+                        <a href="https://www.facebook.com/profile.php?id=100092518021987" target='_blank' className='scl-a'>
+                            <img src={Facebook} alt="" className="scl-lnk-img" />
+                            <h3 className="scl-lbl">Follow me on</h3>
+                            <h3 className="scl-name">Facebook</h3>
+                        </a>
+                        <a href="https://www.instagram.com/ttlha._15/" target='_blank' className='scl-a'>
+                            <img src={Instagram} alt="" className="scl-lnk-img" />
+                            <h3 className="scl-lbl">Follow me on</h3>
+                            <h3 className="scl-name">Instagram</h3>
+                        </a>
+                        <a href="https://github.com/MuhammadTalha15" target='_blank' className='scl-a'>
+                            <img src={Github} alt="" className="scl-lnk-img" />
+                            <h3 className="scl-lbl">Follow me on</h3>
+                            <h3 className="scl-name">Github</h3>
+                        </a>
+                        <a href="https://stackoverflow.com/users/23203699/muhammad-talha" target='_blank' className='scl-a'>
+                            <img src={Stack} alt="" className="scl-lnk-img" />
+                            <h3 className="scl-lbl">Follow me on</h3>
+                            <h3 className="scl-name">Stack-Overflow</h3>
+                        </a>
+                    </div>
+
+                    <Link to="/Contact" className='contact-btn'>Contact Me <span className='arrow'></span></Link>
+
+                </div>
+            </section>
+
+            <Footer/>
+        </>
+    )
 }
 
 export default Home
